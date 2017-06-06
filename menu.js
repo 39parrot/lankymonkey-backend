26db3973acd076e0556900bf3e54035cae8c55a9
dmitry <dmitry.kouznetsov@paxport.se>
2017-06-06 18:28:13 +0200

    /menu

---
+++ menu.js
@@ -0,0 +1,20 @@
+const fetchMenu = restaurantId => {
+  const menuDB = {
+    "aslkdfhdsl":
+      [{
+        name: "asdfasfsda",
+        price: 100
+      }],
+    "KJHLKG":
+      [{
+        name: ";fashdflasf",
+        price: 500
+      }]
+  }
+
+  return menuDB[restaurantId];
+}
+
+module.exports = function(context, cb) {
+  cb(null, fetchMenu(context.query.restaurant))
+}
